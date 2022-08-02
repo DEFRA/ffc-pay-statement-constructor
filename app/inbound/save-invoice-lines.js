19f4e5b1ff3d277c9bf9a51560a776601bfb2f91
Marc Templeton <marcte@kainos.com>
2022-08-02 16:13:04 +0100

    simple savedown successful

--- app/inbound/save-invoice-lines.js
+++ app/inbound/save-invoice-lines.js
@@ -3,6 +3,8 @@ const db = require('../data')
 const saveInvoiceLines = async (invoiceLines, paymentRequestId, transaction) => {
   for (const invoiceLine of invoiceLines) {
     delete invoiceLine.invoiceLineId
+    invoiceLine.fundingCode = invoiceLine.schemeCode
+    delete invoiceLine.schemeCode
     await db.invoiceLine.create({ paymentRequestId, ...invoiceLine }, { transaction })
   }
 }
