a16d4b5743115c06d7a8fe1b53d18cdc408b5ba4
Marc Templeton <marcte@kainos.com>
2022-08-03 17:30:49 +0100

    fixed standard isssues

--- test/unit/processing/reverse-engineer-invoice-number.test.js
+++ test/unit/processing/reverse-engineer-invoice-number.test.js
@@ -2,7 +2,7 @@ const reverseEngineerInvoiceNumber = require('../../../app/processing/reverse-en
 
 let paymentRequest
 
-describe('reverse engineer original invoice number from processed invoice number', () => {  
+describe('reverse engineer original invoice number from processed invoice number', () => {
   beforeEach(() => {
     paymentRequest = JSON.parse(JSON.stringify(require('../../mockPaymentRequest').paymentRequest))
   })
@@ -18,14 +18,14 @@ describe('reverse engineer original invoice number from processed invoice number
   })
 
   test('should return "originalS0000" plus the 5 characters of the invoice number when an invoice number with 5 characters is given', async () => {
-    const invoiceNumber = paymentRequest.invoiceNumber.slice(0,5)
+    const invoiceNumber = paymentRequest.invoiceNumber.slice(0, 5)
     const result = await reverseEngineerInvoiceNumber(invoiceNumber)
-    expect(result).toBe("originalS0000")
+    expect(result).toBe('originalS0000')
   })
 
   test('should return "originalS00" plus the 3 characters of the invoice number when an invoice number with fewer than 5 characters is given', async () => {
-    const invoiceNumber = paymentRequest.invoiceNumber.slice(0,3)
+    const invoiceNumber = paymentRequest.invoiceNumber.slice(0, 3)
     const result = await reverseEngineerInvoiceNumber(invoiceNumber)
-    expect(result).toBe("originalS00")
+    expect(result).toBe('originalS00')
   })
 })
