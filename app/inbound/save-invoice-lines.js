8d4fddea83f773a6b608b3d351e646d562cce9fd
Marc Templeton <marcte@kainos.com>
2022-08-04 11:31:49 +0100

    removed unness deletion of obj key

--- app/inbound/save-invoice-lines.js
+++ app/inbound/save-invoice-lines.js
@@ -4,7 +4,6 @@ const saveInvoiceLines = async (invoiceLines, paymentRequestId, transaction) => 
   for (const invoiceLine of invoiceLines) {
     delete invoiceLine.invoiceLineId
     invoiceLine.fundingCode = invoiceLine.schemeCode
-    delete invoiceLine.schemeCode
     await db.invoiceLine.create({ paymentRequestId, ...invoiceLine }, { transaction })
   }
 }
