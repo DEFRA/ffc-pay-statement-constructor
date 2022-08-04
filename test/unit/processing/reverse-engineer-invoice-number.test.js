d1e867fe5de6d49fbee503db6e526847f394c10b
Marc Templeton <marcte@kainos.com>
2022-08-04 12:09:27 +0100

    fixing wrong reverse eng in test descriptions

--- test/unit/processing/reverse-engineer-invoice-number.test.js
+++ test/unit/processing/reverse-engineer-invoice-number.test.js
@@ -11,19 +11,19 @@ describe('reverse engineer original invoice number from processed invoice number
     jest.clearAllMocks()
   })
 
-  test('should return "originalS0000" plus the first 5 characters of the invoice number when an invoice number with more than 5 characters is given', async () => {
+  test('should return "originalS0000" when an invoice number with more than 5 characters is given', async () => {
     const invoiceNumber = paymentRequest.invoiceNumber
     const result = await reverseEngineerInvoiceNumber(invoiceNumber)
     expect(result).toBe('originalS0000')
   })
 
-  test('should return "originalS0000" plus the 5 characters of the invoice number when an invoice number with 5 characters is given', async () => {
+  test('should return "originalS0000" when an invoice number with 5 characters is given', async () => {
     const invoiceNumber = paymentRequest.invoiceNumber.slice(0, 5)
     const result = await reverseEngineerInvoiceNumber(invoiceNumber)
     expect(result).toBe('originalS0000')
   })
 
-  test('should return "originalS00" plus the 3 characters of the invoice number when an invoice number with fewer than 5 characters is given', async () => {
+  test('should return "originalS00" when an invoice number with fewer than 5 characters is given', async () => {
     const invoiceNumber = paymentRequest.invoiceNumber.slice(0, 3)
     const result = await reverseEngineerInvoiceNumber(invoiceNumber)
     expect(result).toBe('originalS00')
