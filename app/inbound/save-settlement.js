b3d20caafe2624fed2a2c947f61d961938cfe801
Simon Dunn <simondunn90@gmail.com>
2022-08-05 15:46:20 +0100

    remove get-payment-request-by-invoice-number

--- app/inbound/save-settlement.js
+++ app/inbound/save-settlement.js
@@ -1,12 +1,6 @@
 const db = require('../data')
-const getPaymentRequestByInvoiceNumber = require('./get-payment-request-by-invoice-number')
 
 const saveSettlement = async (settlement, transaction) => {
-  // const invoiceNumber = settlement.invoiceNumber
-  // const paymentRequest = await getPaymentRequestByInvoiceNumber(invoiceNumber, transaction)
-  // settlement.paymentRequestId = paymentRequest.paymentRequestId
-  // settlement.schemeId = paymentRequest.schemeId
-
   await db.settlement.create({ ...settlement }, { transaction })
 }
 
