5d1ddd9a9d4245f27653532f451e08dae953cbb7
Simon Dunn <simondunn90@gmail.com>
2022-08-05 11:16:57 +0100

    save settlement data

---
+++ app/inbound/get-payment-request-by-invoice-number.js
@@ -0,0 +1,12 @@
+const db = require('../data')
+const getPaymentRequestByInvoiceNumber = async (invoiceNumber, transaction) => {
+  return db.paymentRequest.findOne({
+    transaction,
+    lock: true,
+    where: {
+      invoiceNumber
+    }
+  })
+}
+
+module.exports = getPaymentRequestByInvoiceNumber
--- app/inbound/save-settlement.js
+++ app/inbound/save-settlement.js
@@ -1,8 +1,17 @@
 const db = require('../data')
+const getPaymentRequestByInvoiceNumber = require('./get-payment-request-by-invoice-number')
 
 const saveSettlement = async (settlement, transaction) => {
-  settlement.paymentRequestId = 1
+  const invoiceNumber = settlement.invoiceNumber
+  const paymentRequest = await getPaymentRequestByInvoiceNumber(invoiceNumber, transaction)
+  settlement.paymentRequestId = paymentRequest.paymentRequestId
+  settlement.schemeId = paymentRequest.schemeId
+
   await db.settlement.create({ ...settlement }, { transaction })
 }
 
 module.exports = saveSettlement
+
+// set the missing fields
+// hard code first
+// then use the invoice number to find them from other tables
