f961e717aa9a7291ddd985c903e4e5e0b14b0dbe
Marc Templeton <marcte@kainos.com>
2022-08-05 09:53:25 +0100

    adding status field to allow us to diff finished prs

--- app/data/models/payment-request.js
+++ app/data/models/payment-request.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     received: DataTypes.DATE,
     referenceId: DataTypes.UUID,
     schedule: DataTypes.STRING,
-    sitiAgriInvoiceNumber: DataTypes.STRING,
+    status: DataTypes.STRING,
     submitted: DataTypes.DATE,
     value: DataTypes.INTEGER
   },
