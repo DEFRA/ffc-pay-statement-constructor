e102d2ea33d4489188e1277e666eda0dad7c02e5
Simon Dunn <simondunn90@gmail.com>
2022-08-30 10:30:24 +0100

    sfi-2200 subscribe to AP settlements (#7)
    * subscribe to AP settlements
    Co-authored-by: Marc Templeton <56440327+marctemp@users.noreply.github.com>

--- app/inbound/index.js
+++ app/inbound/index.js
@@ -1,7 +1,9 @@
 const processProcessingPaymentRequest = require('./processing/process-processing-payment-request')
 const processSubmitPaymentRequest = require('./submit/process-submit-payment-request')
+const processReturnSettlement = require('./process-return-settlement')
 
 module.exports = {
   processProcessingPaymentRequest,
-  processSubmitPaymentRequest
+  processSubmitPaymentRequest,
+  processReturnSettlement
 }
