a5ce3caa5504c27eefffd08bab5cd9c1ff46ffb9
Simon Dunn <simondunn90@gmail.com>
2022-09-02 11:42:17 +0100

    first pass mapping values

--- app/processing/invoice-lines/map-invoice-lines.js
+++ app/processing/invoice-lines/map-invoice-lines.js
@@ -1,13 +1,29 @@
 const groupInvoiceLinesByFundingCode = require('./group-invoice-lines-by-funding-code')
 const mapInvoiceLines = (invoiceLines) => {
   const invoiceLinesGroupedByFundingCode = groupInvoiceLinesByFundingCode(invoiceLines)
-  invoiceLinesGroupedByFundingCode.map(invoiceLineGroup => {
-    const annualPayment = invoiceLineGroup.filter(invoiceLine => invoiceLine.description === 'G00 - Gross value of claim')
-    console.log(annualPayment)
+  const fundingCode = invoiceLinesGroupedByFundingCode[0][0].fundingCode
+  const output = invoiceLinesGroupedByFundingCode.map(invoiceLineGroup => {
+    const annualValue = invoiceLineGroup.find(invoiceLine => invoiceLine.description === 'G00 - Gross value of claim').value
+
+    const reductions = invoiceLineGroup.filter(invoiceLine => invoiceLine.description === 'Over declaration reduction').map(reduction => {
+      const reductionObject = {
+        reason: reduction.description,
+        value: reduction.value
+      }
+      return reductionObject
+    })
+    const totalReduction = reductions.reduce((total, reduction) => { return total += reduction.value }, 0)
+
     return {
-      annualPayment
+      annualValue,
+      fundingCode,
+      reductions,
+      quarterlyValue: annualValue / 4,
+      quarterlyPayment: (annualValue - totalReduction) / 4,
+      quarterlyReduction: totalReduction / 4
     }
   })
+  console.log(output)
 }
 module.exports = mapInvoiceLines
 
