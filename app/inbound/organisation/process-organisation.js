471479eb6eaeb8599b189a9b81afe6172de8918b
John Watson <johnwatson484@hotmail.com>
2022-09-08 19:42:09 +0100

    Remove organisation dup check

--- app/inbound/organisation/process-organisation.js
+++ app/inbound/organisation/process-organisation.js
@@ -1,20 +1,11 @@
 const db = require('../../data')
-
-const getOrganisationBySbi = require('./get-organisation-by-sbi')
 const saveOrganisation = require('./save-organisation')
 
 const processOrganisation = async (organisation) => {
   const transaction = await db.sequelize.transaction()
-
   try {
-    const existingOrganisation = await getOrganisationBySbi(organisation.sbi, transaction)
-    if (existingOrganisation) {
-      console.info(`Duplicate organisation received, skipping ${existingOrganisation.sbi}`)
-      await transaction.rollback()
-    } else {
-      await saveOrganisation(organisation, transaction)
-      await transaction.commit()
-    }
+    await saveOrganisation(organisation, transaction)
+    await transaction.commit()
   } catch (error) {
     await transaction.rollback()
     throw (error)
--- app/inbound/organisation/save-organisation.js
+++ app/inbound/organisation/save-organisation.js
@@ -1,7 +1,7 @@
 const db = require('../../data')
 
 const saveOrganisation = async (organisation, transaction) => {
-  await db.organisation.create(organisation, { transaction })
+  await db.organisation.upsert(organisation, { transaction })
 }
 
 module.exports = saveOrganisation
