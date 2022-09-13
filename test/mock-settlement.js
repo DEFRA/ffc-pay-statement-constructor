88b3bfa10f54386b34de50c70b1cc910d26f210a
Marc Templeton <marcte@kainos.com>
2022-09-13 15:46:43 +0100

    removed needless true constant

--- test/mock-settlement.js
+++ test/mock-settlement.js
@@ -5,7 +5,6 @@ const { STRING_FORMAT: SETTLEMENT_DATE } = require('./mock-components/mock-dates
 const { SETTLEMENT_REFERENCE } = require('./mock-components/mock-settlement-reference')
 const { SFI_FIRST_PAYMENT: SFI_FIRST_PAYMENT_INVOICE_NUMBER } = require('./mock-components/mock-invoice-number')
 const { SOURCE_SYSTEM: SFI_SOURCE_SYSTEM } = require('./mock-components/mock-source-system')
-const TRUE = require('./mock-components/mock-settlement-status')
 
 module.exports = {
   sourceSystem: SFI_SOURCE_SYSTEM,
@@ -15,5 +14,5 @@ module.exports = {
   value: FIVE_HUNDRED_POUNDS,
   settlementDate: SETTLEMENT_DATE,
   reference: SETTLEMENT_REFERENCE,
-  settled: TRUE
+  settled: true
 }
