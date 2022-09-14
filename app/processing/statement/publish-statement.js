394befe938536ae0308e45d2a4e2c653e5182a9d
Marc Templeton <marcte@kainos.com>
2022-09-14 16:34:20 +0100

    didnt commit in last grrrr

--- app/processing/statement/publish-statement.js
+++ app/processing/statement/publish-statement.js
@@ -4,7 +4,7 @@ const config = require('../../config')
 const sendMessage = require('../../messaging/send-message')
 
 const publishStatement = async (statement) => {
-  await sendMessage(statement, 'uk.gov.pay.statement', config.generatorTopic)
+  await sendMessage(statement, 'uk.gov.pay.statement', config.statementTopic)
   console.log('Statement sent:', util.inspect(statement, false, null, true))
 }
 
