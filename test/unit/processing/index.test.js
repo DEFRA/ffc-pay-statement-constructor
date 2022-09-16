cb2628579d5e013988770ce58435f5a920ac495e
Marc Templeton <marcte@kainos.com>
2022-09-16 11:56:17 +0100

    added getStatement unit

--- test/unit/processing/index.test.js
+++ test/unit/processing/index.test.js
@@ -4,15 +4,47 @@ jest.spyOn(global, 'setTimeout')
 jest.mock('../../../app/config')
 const { processingConfig } = require('../../../app/config')
 
-jest.mock('../../../app/processing/schedule/schedule-pending-settlements')
-const schedulePendingSettlements = require('../../../app/processing/schedule/schedule-pending-settlements')
+const mockCommit = jest.fn()
+const mockRollback = jest.fn()
+const mockTransaction = {
+  commit: mockCommit,
+  rollback: mockRollback
+}
+jest.mock('../../../app/data', () => {
+  return {
+    sequelize:
+       {
+         transaction: jest.fn().mockImplementation(() => {
+           return { ...mockTransaction }
+         })
+       }
+  }
+})
+
+jest.mock('../../../app/processing/schedule')
+const schedulePendingSettlements = require('../../../app/processing/schedule')
+
+jest.mock('../../../app/processing/statement')
+const { getStatement, sendStatement } = require('../../../app/processing/statement')
 
 const processing = require('../../../app/processing')
+
+let statement
 
 describe('start processing', () => {
   beforeEach(() => {
     processingConfig.settlementProcessingInterval = 10000
-    schedulePendingSettlements.mockResolvedValue(undefined)
+
+    const schedule = JSON.parse(JSON.stringify(require('../../mock-schedule')))
+    const retreivedSchedule = {
+      scheduleId: 1,
+      settlementId: schedule.settlementId
+    }
+    statement = JSON.parse(JSON.stringify(require('../../mock-objects/mock-statement')))
+
+    schedulePendingSettlements.mockResolvedValue([retreivedSchedule])
+    getStatement.mockResolvedValue(statement)
+    sendStatement.mockResolvedValue(undefined)
   })
 
   afterEach(() => {
@@ -27,6 +59,21 @@ describe('start processing', () => {
   test('should call schedulePendingSettlements once', async () => {
     await processing.start()
     expect(schedulePendingSettlements).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call getStatement when schedulePendingSettlements returns 1 record', async () => {
+    await processing.start()
+    expect(getStatement).toHaveBeenCalled()
+  })
+
+  test('should call getStatement once when schedulePendingSettlements returns 1 record', async () => {
+    await processing.start()
+    expect(getStatement).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call getStatement with schedulePendingSettlements()[0].settlementId and mockTransaction when schedulePendingSettlements returns 1 record', async () => {
+    await processing.start()
+    expect(getStatement).toHaveBeenCalledWith((await schedulePendingSettlements())[0].settlementId, mockTransaction)
   })
 
   test('should call setTimeout', async () => {
