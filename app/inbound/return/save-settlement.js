26eac41c70f484fbaa51a8ac9e431c440de92d22
Simon Dunn <simondunn90@gmail.com>
2022-09-23 10:32:32 +0100

    remove duplicate file and update all references

--- app/inbound/return/save-settlement.js
+++ app/inbound/return/save-settlement.js
@@ -1,8 +1,8 @@
 const db = require('../../data')
-const getPaymentRequestbyInvoiceNumber = require('./get-payment-request-by-invoice-number')
+const getCompletedPaymentRequestbyInvoiceNumber = require('../get-completed-payment-request-by-invoice-number')
 
 const saveSettlement = async (settlement, transaction) => {
-  const matchedPaymentRequest = await getPaymentRequestbyInvoiceNumber(settlement.invoiceNumber, transaction)
+  const matchedPaymentRequest = await getCompletedPaymentRequestbyInvoiceNumber(settlement.invoiceNumber, transaction)
   settlement.paymentRequestId = (matchedPaymentRequest ? matchedPaymentRequest?.paymentRequestId : null) ?? null
   await db.settlement.create(settlement, { transaction })
 }
