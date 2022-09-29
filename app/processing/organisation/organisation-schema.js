65f96720b56d1c446042df55c6a12a9bc1049bfa
Abidemi Adio <abidemi.adio@defra.gov.uk>
2022-09-29 14:08:17 +0100

    Modify organisation schema to correspond with statement-data structure

--- app/processing/organisation/organisation-schema.js
+++ app/processing/organisation/organisation-schema.js
@@ -1,14 +1,14 @@
 const Joi = require('joi')
 
 module.exports = Joi.object({
-  sbi: Joi.number().integer().required(),
-  addressLine1: Joi.string().required(),
-  addressLine2: Joi.string().allow('').allow(null).default(''),
-  addressLine3: Joi.string().required(),
-  city: Joi.string().required(),
-  county: Joi.string().allow('').allow(null).default(''),
-  emailAddress: Joi.string().required(),
-  frn: Joi.number().integer().required(),
+  sbi: Joi.number().integer().min(105000000).max(999999999).required(),
+  addressLine1: Joi.string().optional(),
+  addressLine2: Joi.string().optional(),
+  addressLine3: Joi.string().optional(),
+  city: Joi.string().optional(),
+  county: Joi.string().optional(),
+  emailAddress: Joi.string().email().required(),
+  frn: Joi.number().integer().min(1000000000).max(9999999999).required(),
   name: Joi.string().required(),
   postcode: Joi.string().required()
 }).required()
