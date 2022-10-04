4a0e231dd31cdab29799dabc550f101fbcd19be3
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-10-04 15:37:53 +0100

    Fix reverse engineer invoice numbers (#28)

--- app/processing/organisation/organisation-schema.js
+++ app/processing/organisation/organisation-schema.js
@@ -2,11 +2,11 @@ const Joi = require('joi')
 
 module.exports = Joi.object({
   sbi: Joi.number().integer().min(105000000).max(999999999).required(),
-  addressLine1: Joi.string().optional(),
-  addressLine2: Joi.string().optional(),
-  addressLine3: Joi.string().optional(),
-  city: Joi.string().optional(),
-  county: Joi.string().optional(),
+  addressLine1: Joi.string().allow('', null).optional(),
+  addressLine2: Joi.string().allow('', null).optional(),
+  addressLine3: Joi.string().allow('', null).optional(),
+  city: Joi.string().allow('', null).optional(),
+  county: Joi.string().allow('', null).optional(),
   emailAddress: Joi.string().email().required(),
   frn: Joi.number().integer().min(1000000000).max(9999999999).required(),
   name: Joi.string().required(),
