807e76818477922fb432d3998e84ca1f14ee7e82
John Watson <johnwatson484@hotmail.com>
2022-10-12 20:27:44 +0100

    Add tests

--- test/integration/processing/settlement/get-last-settlement.test.js
+++ test/integration/processing/settlement/get-last-settlement.test.js
@@ -1,6 +1,7 @@
 const db = require('../../../../app/data')
 
 const { getLastSettlement } = require('../../../../app/processing/settlement')
+const { SETTLEMENT } = require('../../../mock-components/mock-dates')
 let currentSettlement
 let previousSettlement
 
@@ -14,7 +15,9 @@ describe('process settlement', () => {
 
   beforeEach(async () => {
     currentSettlement = JSON.parse(JSON.stringify(require('../../../mock-settlement')))
+    currentSettlement.settlementDate = new Date(SETTLEMENT.DATE)
     previousSettlement = JSON.parse(JSON.stringify(require('../../../mock-settlement')))
+    previousSettlement.settlementDate = new Date(SETTLEMENT.DATE - 1)
   })
 
   afterEach(async () => {
@@ -40,6 +43,7 @@ describe('process settlement', () => {
   })
 
   test('should return null if previous settlements same date', async () => {
+    previousSettlement.settledDate = currentSettlement.settledDate
     previousSettlement.value = 40000
     await db.settlement.create(previousSettlement)
     const lastSettlement = await getLastSettlement(currentSettlement)
@@ -47,22 +51,28 @@ describe('process settlement', () => {
   })
 
   test('should return null if previous settlements same value and earlier date', async () => {
-    previousSettlement.settledDate = 'Tue Feb 07 2022 00:00:00 GMT+0000 (Greenwich Mean Time)'
     await db.settlement.create(previousSettlement)
     const lastSettlement = await getLastSettlement(currentSettlement)
     expect(lastSettlement).toBeNull()
   })
 
   test('should return null if previous settlements lower value and later date', async () => {
-    previousSettlement.settledDate = 'Tue Feb 09 2022 00:00:00 GMT+0000 (Greenwich Mean Time)'
+    previousSettlement.settledDate = new Date(SETTLEMENT.DATE + 1)
     previousSettlement.value = 40000
     await db.settlement.create(previousSettlement)
     const lastSettlement = await getLastSettlement(currentSettlement)
     expect(lastSettlement).toBeNull()
   })
 
+  test('should return null if previous settlements unsettled', async () => {
+    previousSettlement.value = 40000
+    previousSettlement.settled = false
+    await db.settlement.create(previousSettlement)
+    const lastSettlement = await getLastSettlement(currentSettlement)
+    expect(lastSettlement).toBeNull()
+  })
+
   test('should return settlement if previous settlements lower value and earlier date', async () => {
-    previousSettlement.settledDate = 'Tue Feb 07 2022 00:00:00 GMT+0000 (Greenwich Mean Time)'
     previousSettlement.value = 40000
     await db.settlement.create(previousSettlement)
     const lastSettlement = await getLastSettlement(currentSettlement)
