d8c99129fabb24adc74232ceefe99659b37c6366
John Watson <johnwatson484@hotmail.com>
2022-10-13 15:54:50 +0100

    Calculate reductions

--- app/processing/invoice-line/get-invoice-line.js
+++ app/processing/invoice-line/get-invoice-line.js
@@ -1,26 +1,42 @@
 const schema = require('./invoice-line-schema')
 
 const getPositiveInvoiceLineByFundingCodeAndPaymentRequestId = require('./get-positive-invoice-line-by-funding-code-and-payment-request-id')
+const getNegativeInvoiceLineByFundingCodeAndPaymentRequestId = require('./get-negative-invoice-lines-by-funding-code-and-payment-request-id')
 
 const QUARTER = 0.25
 const MIN_PAYMENT_VALUE = 0
-const DEFAULT_REDUCTION_VALUE = 0
 
 const getInvoiceLine = async (fundingCode, paymentRequestId) => {
-  const invoiceLine = await getPositiveInvoiceLineByFundingCodeAndPaymentRequestId(fundingCode, paymentRequestId)
-  const result = schema.validate(invoiceLine, {
+  const grossValueInvoiceLine = await getPositiveInvoiceLineByFundingCodeAndPaymentRequestId(fundingCode, paymentRequestId)
+  const result = schema.validate(grossValueInvoiceLine, {
     abortEarly: false
   })
 
   if (result.error) {
-    throw new Error(`Payment request with paymentRequestId: ${paymentRequestId} does not have the required invoice-line data for funding code ${fundingCode} : ${result.error.message}`)
+    throw new Error(`Payment request with paymentRequestId: ${paymentRequestId} does not have the required gross invoice-line data for funding code ${fundingCode} : ${result.error.message}`)
   }
 
-  const annualValue = invoiceLine.value
-  const quarterlyValue = annualValue > MIN_PAYMENT_VALUE ? Math.trunc(annualValue * QUARTER) : MIN_PAYMENT_VALUE
-  const quarterlyReduction = DEFAULT_REDUCTION_VALUE
-  const quarterlyPayment = quarterlyValue - quarterlyReduction
   const reductions = []
+  let annualReduction = 0
+  const reductionInvoiceLines = await getNegativeInvoiceLineByFundingCodeAndPaymentRequestId(fundingCode, paymentRequestId)
+  for (const reductionInvoiceLine of reductionInvoiceLines) {
+    const reductionResult = schema.validate(reductionInvoiceLine, {
+      abortEarly: false
+    })
+    if (reductionResult.error) {
+      throw new Error(`Payment request with paymentRequestId: ${paymentRequestId} does not have the required reduction invoice-line data for funding code ${fundingCode} : ${result.error.message}`)
+    }
+    reductions.push({
+      reason: reductionInvoiceLine.description,
+      value: reductionInvoiceLine.value
+    })
+    annualReduction += reductionInvoiceLine.value
+  }
+
+  const annualValue = grossValueInvoiceLine.value
+  const quarterlyValue = annualValue > MIN_PAYMENT_VALUE ? Math.trunc(annualValue * QUARTER) : MIN_PAYMENT_VALUE
+  const quarterlyReduction = Math.trunc(annualReduction * QUARTER)
+  const quarterlyPayment = quarterlyValue - quarterlyReduction
 
   return {
     annualValue,
