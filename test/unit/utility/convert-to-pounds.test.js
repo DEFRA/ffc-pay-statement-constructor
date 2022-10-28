c2c719913766284b3bf02c9a12a0430d6d1b37d3
John Watson <johnwatson484@hotmail.com>
2022-10-28 09:49:43 +0100

    Add tests

--- test/unit/utility/convert-to-pounds.test.js
+++ test/unit/utility/convert-to-pounds.test.js
@@ -9,4 +9,79 @@ describe('convert integer pence to string decimal pounds', () => {
     const result = await convertToPounds(56789)
     expect(result).toBe('567.89')
   })
+
+  test('should return "0.00" when 0 is given', async () => {
+    const result = await convertToPounds(0)
+    expect(result).toBe('0.00')
+  })
+
+  test('should return "0.01" when 1 is given', async () => {
+    const result = await convertToPounds(1)
+    expect(result).toBe('0.01')
+  })
+
+  test('should return "0.10" when 10 is given', async () => {
+    const result = await convertToPounds(10)
+    expect(result).toBe('0.10')
+  })
+
+  test('should return "1.00" when 100 is given', async () => {
+    const result = await convertToPounds(100)
+    expect(result).toBe('1.00')
+  })
+
+  test('should return "1.01" when 101 is given', async () => {
+    const result = await convertToPounds(101)
+    expect(result).toBe('1.01')
+  })
+
+  test('should return "1.10" when 110 is given', async () => {
+    const result = await convertToPounds(110)
+    expect(result).toBe('1.10')
+  })
+
+  test('should return "1.11" when 111 is given', async () => {
+    const result = await convertToPounds(111)
+    expect(result).toBe('1.11')
+  })
+
+  test('should return "10.00" when 1000 is given', async () => {
+    const result = await convertToPounds(1000)
+    expect(result).toBe('10.00')
+  })
+
+  test('should return "10.01" when 1001 is given', async () => {
+    const result = await convertToPounds(1001)
+    expect(result).toBe('10.01')
+  })
+
+  test('should return "10.10" when 1010 is given', async () => {
+    const result = await convertToPounds(1010)
+    expect(result).toBe('10.10')
+  })
+
+  test('should return "10.11" when 1011 is given', async () => {
+    const result = await convertToPounds(1011)
+    expect(result).toBe('10.11')
+  })
+
+  test('should return "11.00" when 1100 is given', async () => {
+    const result = await convertToPounds(1100)
+    expect(result).toBe('11.00')
+  })
+
+  test('should return "11.02" when 1101.1 is given', async () => {
+    const result = await convertToPounds(1101.1)
+    expect(result).toBe('11.02')
+  })
+
+  test('should return "11.02" when 1101.5 is given', async () => {
+    const result = await convertToPounds(1101.1)
+    expect(result).toBe('11.02')
+  })
+
+  test('should return "11.02" when 1101.9 is given', async () => {
+    const result = await convertToPounds(1101.1)
+    expect(result).toBe('11.02')
+  })
 })
