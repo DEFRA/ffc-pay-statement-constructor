f9dd7e43fd07125f93de52be464497c0ad543120
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-11-11 13:07:11 +0000

    Fix payment period (#42)
    * Fix payment period
    
    * Fix period dates

--- app/processing/payment/get-period-from-schedule.js
+++ app/processing/payment/get-period-from-schedule.js
@@ -4,7 +4,9 @@ const getPeriodFromSchedule = (totalValue, totalPayments, paymentValue, schedule
   const firstPeriodIndex = schedule.findIndex(x => x.outstanding)
   const firstPayment = schedule[firstPeriodIndex]
   const lastPayment = schedule[firstPeriodIndex + incrementsInPayment]
-  return `${firstPayment.dueDate.format('MMMM')} to ${lastPayment.dueDate.add(increment - 1, unit).format('MMMM YYYY')}`
+  const firstPeriod = firstPayment.dueDate.clone().subtract(increment, unit)
+  const lastPeriod = lastPayment.dueDate.clone().subtract(1, unit)
+  return `${firstPeriod.format('MMMM')} to ${lastPeriod.format('MMMM YYYY')}`
 }
 
 module.exports = getPeriodFromSchedule
