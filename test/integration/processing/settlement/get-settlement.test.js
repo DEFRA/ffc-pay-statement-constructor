facb8cd88a56c412d8a149013ec20b52694ae3b7
John Watson <johnwatson484@hotmail.com>
2022-11-12 23:48:13 +0000

    Add test

--- test/integration/processing/settlement/get-settlement.test.js
+++ test/integration/processing/settlement/get-settlement.test.js
@@ -1,6 +1,8 @@
 const db = require('../../../../app/data')
-
+const moment = require('moment')
 const { getSettlement } = require('../../../../app/processing/settlement')
+const { ONE_HUNDRED_POUNDS, TWO_HUNDRED_POUNDS, ONE_THOUSAND_POUNDS } = require('../../../mock-components/mock-value')
+const { DATE: SETTLEMENT_DATE } = require('../../../mock-components/mock-dates').SETTLEMENT
 
 const SETTLEMENT_ID_NOT_SETTLED = 1
 const SETTLEMENT_ID_SETTLED = 2
@@ -24,6 +26,7 @@ describe('process settlement', () => {
     } = JSON.parse(JSON.stringify(require('../../../mock-components/mock-invoice-number')))
     const paymentRequest = JSON.parse(JSON.stringify(require('../../../mock-objects/mock-payment-request').submitPaymentRequest))
     settlement = JSON.parse(JSON.stringify(require('../../../mock-objects/mock-settlement')))
+    settlement.settlementDate = SETTLEMENT_DATE
 
     await db.scheme.bulkCreate(schemes)
     await db.invoiceNumber.create({ invoiceNumber, originalInvoiceNumber })
@@ -60,87 +63,98 @@ describe('process settlement', () => {
   })
 
   test('should return paymentValue as settlement value if no previous settlements', async () => {
-    await db.settlement.create({ ...settlement })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
+    await db.settlement.create(settlement)
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
     expect(result.paymentValue).toBe(settlement.value)
   })
 
   test('should return lastSettlementValue as 0 if no previous settlements', async () => {
-    await db.settlement.create({ ...settlement })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
+    await db.settlement.create(settlement)
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
     expect(result.lastSettlementValue).toBe(0)
   })
 
   test('should return paymentValue as settlement value if previous settlements same date and value', async () => {
-    await db.settlement.create({ ...settlement })
-    await db.settlement.create({ ...settlement })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
+    await db.settlement.create(settlement)
+    settlement.settlementDate = moment(settlement.settlementDate).subtract(1, 'day')
+    await db.settlement.create(settlement)
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
     expect(result.paymentValue).toBe(settlement.value)
   })
 
   test('should return lastSettlementValue as 0 if previous settlements same date and value', async () => {
-    await db.settlement.create({ ...settlement })
-    await db.settlement.create({ ...settlement })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
+    await db.settlement.create(settlement)
+    await db.settlement.create(settlement)
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
     expect(result.lastSettlementValue).toBe(0)
   })
 
   test('should return paymentValue as settlement value if previous settlement has higher value', async () => {
-    await db.settlement.create({ ...settlement })
-    await db.settlement.create({ ...settlement, value: 1000 })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
+    await db.settlement.create(settlement)
+    settlement.settlementDate = moment(settlement.settlementDate).subtract(1, 'day')
+    await db.settlement.create({ ...settlement, value: ONE_THOUSAND_POUNDS })
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
     expect(result.paymentValue).toBe(settlement.value)
   })
 
   test('should return lastSettlementValue as 0 if previous settlement has higher value', async () => {
-    await db.settlement.create({ ...settlement })
-    await db.settlement.create({ ...settlement, value: 1000 })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
+    await db.settlement.create(settlement)
+    settlement.settlementDate = moment(settlement.settlementDate).subtract(1, 'day')
+    await db.settlement.create({ ...settlement, value: ONE_THOUSAND_POUNDS })
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
     expect(result.lastSettlementValue).toBe(0)
   })
 
   test('should return paymentValue as settlement value if previous settlement unsettled', async () => {
-    await db.settlement.create({ ...settlement })
+    await db.settlement.create(settlement)
+    settlement.settlementDate = moment(settlement.settlementDate).subtract(1, 'day')
     await db.settlement.create({ ...settlement, settled: false })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
     expect(result.paymentValue).toBe(settlement.value)
   })
 
   test('should return lastSettlementValue as 0 if previous settlement unsettled', async () => {
-    await db.settlement.create({ ...settlement })
+    await db.settlement.create(settlement)
+    settlement.settlementDate = moment(settlement.settlementDate).subtract(1, 'day')
     await db.settlement.create({ ...settlement, settled: false })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
     expect(result.lastSettlementValue).toBe(0)
   })
 
   test('should return paymentValue subtracting previous settlement', async () => {
-    await db.settlement.create({ ...settlement })
-    await db.settlement.create({ ...settlement, value: 100 })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
-    expect(result.paymentValue).toBe(settlement.value - 100)
+    await db.settlement.create(settlement)
+    settlement.settlementDate = moment(settlement.settlementDate).subtract(1, 'day')
+    await db.settlement.create({ ...settlement, value: ONE_HUNDRED_POUNDS })
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
+    expect(result.paymentValue).toBe(settlement.value - ONE_HUNDRED_POUNDS)
   })
 
   test('should return lastSettlementValue as previous settlement value', async () => {
-    await db.settlement.create({ ...settlement })
-    await db.settlement.create({ ...settlement, value: 100 })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
-    expect(result.lastSettlementValue).toBe(100)
+    await db.settlement.create(settlement)
+    settlement.settlementDate = moment(settlement.settlementDate).subtract(1, 'day')
+    await db.settlement.create({ ...settlement, value: ONE_HUNDRED_POUNDS })
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
+    expect(result.lastSettlementValue).toBe(ONE_HUNDRED_POUNDS)
   })
 
   test('should return paymentValue subtracting latest previous settlement if multiple previous settlements', async () => {
-    await db.settlement.create({ ...settlement })
-    await db.settlement.create({ ...settlement, value: 100 })
-    await db.settlement.create({ ...settlement, value: 200 })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
-    expect(result.paymentValue).toBe(settlement.value - 200)
+    await db.settlement.create(settlement)
+    settlement.settlementDate = moment(settlement.settlementDate).subtract(2, 'day')
+    await db.settlement.create({ ...settlement, value: ONE_HUNDRED_POUNDS })
+    settlement.settlementDate = settlement.settlementDate.add(1, 'day')
+    await db.settlement.create({ ...settlement, value: TWO_HUNDRED_POUNDS })
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
+    expect(result.paymentValue).toBe(settlement.value - TWO_HUNDRED_POUNDS)
   })
 
   test('should return lastSettlementValue as latest previous settlement value if multiple previous settlements', async () => {
-    await db.settlement.create({ ...settlement })
-    await db.settlement.create({ ...settlement, value: 100 })
-    await db.settlement.create({ ...settlement, value: 200 })
-    const result = await getSettlement(SETTLEMENT_ID_NOT_SETTLED)
-    expect(result.lastSettlementValue).toBe(200)
+    await db.settlement.create(settlement)
+    settlement.settlementDate = moment(settlement.settlementDate).subtract(2, 'day')
+    await db.settlement.create({ ...settlement, value: ONE_HUNDRED_POUNDS })
+    settlement.settlementDate = settlement.settlementDate.add(1, 'day')
+    await db.settlement.create({ ...settlement, value: TWO_HUNDRED_POUNDS })
+    const result = await getSettlement(SETTLEMENT_ID_SETTLED)
+    expect(result.lastSettlementValue).toBe(TWO_HUNDRED_POUNDS)
   })
 
   test('should throw when no existing settlement exists', async () => {
