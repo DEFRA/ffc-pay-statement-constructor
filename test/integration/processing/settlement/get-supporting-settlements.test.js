0a488fbb0c98d5285a4fac4a1e2ffeba91aab209
John Watson <johnwatson484@hotmail.com>
2022-11-12 16:41:15 +0000

    Add tests

--- test/integration/processing/settlement/get-supporting-settlements.test.js
+++ test/integration/processing/settlement/get-supporting-settlements.test.js
@@ -7,6 +7,7 @@ const invoiceNumbers = require('../../../mock-components/mock-invoice-number')
 const { SFI: SFI_AGREEMENT_NUMBER } = require('../../../mock-components/mock-agreement-number')
 const MARKETING_YEAR = require('../../../mock-components/mock-marketing-year')
 const { DATE: SETTLEMENT_DATE } = require('../../../mock-components/mock-dates').SETTLEMENT
+const schemes = require('../../../../app/constants/schemes')
 
 let settlementDate
 let agreementNumber
@@ -16,8 +17,6 @@ let paymentRequest
 let topUpPaymentRequest
 let downwardAdjustmentPaymentRequest
 let splitPaymentRequestA
-let splitPaymentRequestB
-let recoveryPaymentRequest
 
 let settlement
 
@@ -34,8 +33,6 @@ describe('get supporting settlements', () => {
     topUpPaymentRequest = JSON.parse(JSON.stringify(require('../../../mock-objects/mock-payment-request').topUpSubmitPaymentRequest))
     downwardAdjustmentPaymentRequest = JSON.parse(JSON.stringify(require('../../../mock-objects/mock-payment-request').downwardAdjustmentSubmitPaymentRequest))
     splitPaymentRequestA = JSON.parse(JSON.stringify(require('../../../mock-objects/mock-payment-request').splitSubmitPaymentRequestA))
-    splitPaymentRequestB = JSON.parse(JSON.stringify(require('../../../mock-objects/mock-payment-request').splitSubmitPaymentRequestB))
-    recoveryPaymentRequest = JSON.parse(JSON.stringify(require('../../../mock-objects/mock-payment-request').recoverySubmitPaymentRequest))
 
     settlement = JSON.parse(JSON.stringify(require('../../../mock-objects/mock-settlement')))
     settlement.settlementDate = SETTLEMENT_DATE
@@ -44,7 +41,10 @@ describe('get supporting settlements', () => {
     agreementNumber = SFI_AGREEMENT_NUMBER
     marketingYear = MARKETING_YEAR
 
+    await db.scheme.bulkCreate(schemes)
     await db.settlement.create(settlement)
+
+    delete paymentRequest.paymentRequestId
   })
 
   afterEach(async () => {
@@ -59,6 +59,40 @@ describe('get supporting settlements', () => {
   })
 
   test('should return empty array if no completed payment requests', async () => {
+    const settlements = await getSupportingSettlements(settlementDate, agreementNumber, marketingYear)
+    expect(settlements).toHaveLength(0)
+  })
+
+  test('should return empty array if only completed is first payment', async () => {
+    await db.invoiceNumber.create({ invoiceNumber: invoiceNumbers.SFI_FIRST_PAYMENT, originalInvoiceNumber: invoiceNumbers.SFI_FIRST_PAYMENT_ORIGINAL })
+    await db.paymentRequest.create(paymentRequest)
+    const settlements = await getSupportingSettlements(settlementDate, agreementNumber, marketingYear)
+    expect(settlements).toHaveLength(0)
+  })
+
+  test('should return empty array if top up has no settlement', async () => {
+    await db.invoiceNumber.create({ invoiceNumber: invoiceNumbers.SFI_FIRST_PAYMENT, originalInvoiceNumber: invoiceNumbers.SFI_FIRST_PAYMENT_ORIGINAL })
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceNumber.create({ invoiceNumber: invoiceNumbers.SFI_SECOND_PAYMENT, originalInvoiceNumber: invoiceNumbers.SFI_SECOND_PAYMENT_ORIGINAL })
+    await db.paymentRequest.create(topUpPaymentRequest)
+    const settlements = await getSupportingSettlements(settlementDate, agreementNumber, marketingYear)
+    expect(settlements).toHaveLength(0)
+  })
+
+  test('should return empty array if downward adjustment has no settlement', async () => {
+    await db.invoiceNumber.create({ invoiceNumber: invoiceNumbers.SFI_FIRST_PAYMENT, originalInvoiceNumber: invoiceNumbers.SFI_FIRST_PAYMENT_ORIGINAL })
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceNumber.create({ invoiceNumber: invoiceNumbers.SFI_SECOND_PAYMENT, originalInvoiceNumber: invoiceNumbers.SFI_SECOND_PAYMENT_ORIGINAL })
+    await db.paymentRequest.create(downwardAdjustmentPaymentRequest)
+    const settlements = await getSupportingSettlements(settlementDate, agreementNumber, marketingYear)
+    expect(settlements).toHaveLength(0)
+  })
+
+  test('should return empty array if split payment has no settlement', async () => {
+    await db.invoiceNumber.create({ invoiceNumber: invoiceNumbers.SFI_FIRST_PAYMENT, originalInvoiceNumber: invoiceNumbers.SFI_FIRST_PAYMENT_ORIGINAL })
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceNumber.create({ invoiceNumber: invoiceNumbers.SFI_SPLIT_A, originalInvoiceNumber: invoiceNumbers.SFI_SECOND_PAYMENT_ORIGINAL })
+    await db.paymentRequest.create(splitPaymentRequestA)
     const settlements = await getSupportingSettlements(settlementDate, agreementNumber, marketingYear)
     expect(settlements).toHaveLength(0)
   })
