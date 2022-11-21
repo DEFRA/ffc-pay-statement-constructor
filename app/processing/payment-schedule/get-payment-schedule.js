328caa393bf3cb65abb058b4d671970d324261ad
John Watson <johnwatson484@hotmail.com>
2022-11-21 11:41:30 +0000

    Correct frequency

--- app/processing/payment-schedule/get-payment-schedule.js
+++ app/processing/payment-schedule/get-payment-schedule.js
@@ -2,17 +2,19 @@ const db = require('../../data')
 const getCalculation = require('../calculation')
 const getInProgressPaymentRequest = require('../payment-request/get-in-progress-payment-request')
 const { getDetails, getAddress, getScheme } = require('../statement/components')
+const mapPaymentRequest = require('../payment-request/map-payment-request')
 
 const getPaymentSchedule = async (paymentRequestId) => {
   const transaction = await db.sequelize.transaction()
   try {
     const paymentRequest = await db.paymentRequest.findByPk(paymentRequestId, { transaction })
     const inProgressPaymentRequest = await getInProgressPaymentRequest(paymentRequest.correlationId, transaction)
-    const calculation = await getCalculation(inProgressPaymentRequest.paymentRequestId, inProgressPaymentRequest.invoiceNumber, transaction)
+    const mappedPaymentRequest = mapPaymentRequest(inProgressPaymentRequest)
+    const calculation = await getCalculation(mappedPaymentRequest.paymentRequestId, mappedPaymentRequest.invoiceNumber, transaction)
     const sbi = calculation.sbi
     const details = await getDetails(sbi, transaction)
     const address = await getAddress(sbi, transaction)
-    const scheme = getScheme(inProgressPaymentRequest.year, inProgressPaymentRequest.frequency, inProgressPaymentRequest.agreementNumber)
+    const scheme = getScheme(mappedPaymentRequest.year, mappedPaymentRequest.frequency, mappedPaymentRequest.agreementNumber)
 
     await transaction.commit()
     return {
--- app/processing/schedule/index.js
+++ app/processing/schedule/index.js
@@ -1,4 +1,4 @@
-const { schedulePendingSettlements } = require('./schedule-pending-settlements')
+const schedulePendingSettlements = require('./schedule-pending-settlements')
 const schedulePendingPaymentSchedules = require('./schedule-pending-payment-schedules')
 
 module.exports = {
