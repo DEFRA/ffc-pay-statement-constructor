df660dce4ee7f74bbe75ea34828b03c89cff9002
Marc Templeton <marcte@kainos.com>
2023-01-11 12:11:48 +0000

    removed redun code and app config

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,7 +1,5 @@
 jest.mock('../../app/messaging')
 const mockMessaging = require('../../app/messaging')
-jest.mock('../../app/storage')
-const mockStorage = require('../../app/storage')
 
 describe('app', () => {
   beforeEach(() => {
@@ -10,9 +8,5 @@ describe('app', () => {
 
   test('starts messaging', async () => {
     expect(mockMessaging.start).toHaveBeenCalled()
-  })
-
-  test('initialises containers', async () => {
-    expect(mockStorage.initialiseContainers).toHaveBeenCalled()
   })
 })
