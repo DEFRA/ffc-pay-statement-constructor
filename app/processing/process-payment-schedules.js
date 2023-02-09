b2e4fcfe58606e27025afabe5bba00a7fac61921
John Watson <johnwatson484@hotmail.com>
2023-02-09 20:53:19 +0000

    Filter out zero changes

--- app/processing/process-payment-schedules.js
+++ app/processing/process-payment-schedules.js
@@ -1,4 +1,4 @@
-const { getPaymentSchedule, sendPaymentSchedule } = require('./payment-schedule')
+const { getPaymentSchedule, sendPaymentSchedule, validatePaymentSchedule } = require('./payment-schedule')
 const { schedulePendingPaymentSchedules } = require('./schedule')
 const updateScheduleByScheduleId = require('./update-schedule-by-schedule-id')
 
@@ -8,7 +8,9 @@ const processPaymentSchedules = async () => {
   for (const pendingPaymentSchedule of pendingPaymentSchedules) {
     try {
       const aggregatedSchedule = await getPaymentSchedule(pendingPaymentSchedule.paymentRequestId)
-      await sendPaymentSchedule(aggregatedSchedule)
+      if (validatePaymentSchedule(aggregatedSchedule)) {
+        await sendPaymentSchedule(aggregatedSchedule)
+      }
       await updateScheduleByScheduleId(pendingPaymentSchedule.scheduleId)
     } catch (err) {
       console.error(err.message)
