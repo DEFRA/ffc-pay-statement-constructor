cd77d34f3c731260e92e682ee86e7cc7dbbbe340
John Watson <john.watson2@rpa.gov.uk>
2023-02-10 12:22:53 +0000

    Calculate delta

--- app/processing/payment/index.js
+++ app/processing/payment/index.js
@@ -1,7 +1,9 @@
 const getLatestPayment = require('./get-latest-payment')
 const { calculateScheduledPayments } = require('./calculate-scheduled-payments')
+const calculateDelta = require('./calculate-delta')
 
 module.exports = {
   getLatestPayment,
-  calculateScheduledPayments
+  calculateScheduledPayments,
+  calculateDelta
 }
