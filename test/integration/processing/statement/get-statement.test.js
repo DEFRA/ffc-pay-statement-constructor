0370a22b491806d947f153610afc107660274502
Marc Templeton <marcte@kainos.com>
2023-05-19 15:14:17 +0100

    statement complete tests added

--- test/integration/processing/statement/get-statement.test.js
+++ test/integration/processing/statement/get-statement.test.js
@@ -67,7 +67,187 @@ describe('get statement', () => {
   })
 
   describe('when all info', () => {
-    test('return what I want', async () => {
+    test('returns frn key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res)).toContain('frn')
+    })
+
+    test('returns frn as number', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.frn).toEqual(expect.any(Number))
+    })
+
+    test('returns frn as statement.frn', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.frn).toEqual(statement.frn)
+    })
+
+    test('returns sbi key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res)).toContain('sbi')
+    })
+
+    test('returns sbi as number', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.sbi).toEqual(expect.any(Number))
+    })
+
+    test('returns sbi as statement.sbi', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.sbi).toEqual(statement.sbi)
+    })
+
+    test('returns funding key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res)).toContain('funding')
+    })
+
+    test('returns funding as array', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding).toEqual(expect.any(Array))
+    })
+
+    test('returns annualValue key within funding key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res.funding[0])).toContain('annualValue')
+    })
+
+    test('returns annualValue as string', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].annualValue).toEqual(expect.any(String))
+    })
+
+    test('returns annualValue as statement.funding.annualValue', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].annualValue).toEqual(statement.funding[0].annualValue)
+    })
+
+    test('returns area key within funding key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res.funding[0])).toContain('area')
+    })
+
+    test('returns area as string', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].area).toEqual(expect.any(String))
+    })
+
+    test('returns area as statement.funding.area', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].area).toEqual(statement.funding[0].area)
+    })
+
+    test('returns quarterlyPayment key within funding key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res.funding[0])).toContain('quarterlyPayment')
+    })
+
+    test('returns quarterlyPayment as string', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].quarterlyPayment).toEqual(expect.any(String))
+    })
+
+    test('returns quarterlyPayment as statement.funding.quarterlyPayment', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].quarterlyPayment).toEqual(statement.funding[0].quarterlyPayment)
+    })
+
+    test('returns quarterlyReduction key within funding key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res.funding[0])).toContain('quarterlyReduction')
+    })
+
+    test('returns quarterlyReduction as string', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].quarterlyReduction).toEqual(expect.any(String))
+    })
+
+    test('returns quarterlyReduction as statement.funding.quarterlyReduction', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].quarterlyReduction).toEqual(statement.funding[0].quarterlyReduction)
+    })
+
+    test('returns quarterlyValue key within funding key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res.funding[0])).toContain('quarterlyValue')
+    })
+
+    test('returns quarterlyValue as string', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].quarterlyValue).toEqual(expect.any(String))
+    })
+
+    test('returns quarterlyValue as statement.funding.quarterlyValue', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].quarterlyValue).toEqual(statement.funding[0].quarterlyValue)
+    })
+
+    test('returns rate key within funding key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res.funding[0])).toContain('rate')
+    })
+
+    test('returns rate as string', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].rate).toEqual(expect.any(String))
+    })
+
+    test('returns rate as statement.funding.rate', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.funding[0].rate).toEqual(statement.funding[0].rate)
+    })
+
+    test('returns payments key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res)).toContain('payments')
+    })
+
+    test('returns payments as array', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.payments).toEqual(expect.any(Array))
+    })
+
+    test('returns value key within payments key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res.payments[0])).toContain('value')
+    })
+
+    test('returns value as string', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.payments[0].value).toEqual(expect.any(String))
+    })
+
+    test('returns value as statement.payments.value', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.payments[0].value).toEqual(statement.payments[0].value)
+    })
+
+    test('returns scheme key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res)).toContain('scheme')
+    })
+
+    test('returns scheme as object', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.scheme).toEqual(expect.any(Object))
+    })
+
+    test('returns year key within scheme key', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(Object.keys(res.scheme)).toContain('year')
+    })
+
+    test('returns year as string', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.scheme.year).toEqual(expect.any(String))
+    })
+
+    test('returns year as statement.scheme.year', async () => {
+      const res = await getStatement(schedule.settlementId, schedule.scheduleId)
+      expect(res.scheme.year).toEqual(statement.scheme.year)
+    })
+
+    test('returns statement', async () => {
       const res = await getStatement(schedule.settlementId, schedule.scheduleId)
       expect(res).toStrictEqual(statement)
     })
