a69b962e6d64654b50422f3b0d2951b930371820
Marc Templeton <marcte@kainos.com>
2023-05-23 10:35:57 +0100

    process statement

--- app/processing/statement/publish-statement.js
+++ app/processing/statement/publish-statement.js
@@ -5,7 +5,7 @@ const sendMessage = require('../../messaging/send-message')
 
 const publishStatement = async (statement) => {
   await sendMessage(statement, 'uk.gov.pay.statement', config.statementTopic)
-  console.log('Statement sent:', util.inspect(statement, false, null, true))
+  // console.log('Statement sent:', util.inspect(statement, false, null, true))
 }
 
 module.exports = publishStatement
--- test/integration/processing/statement/process-statements.test.js
+++ test/integration/processing/statement/process-statements.test.js
@@ -1,9 +1,8 @@
 const db = require('../../../../app/data')
 
-// jest mock timers
-/// jest system time now
+const { mockMessageSender } = require('../../../mock-modules/ffc-messaging')
 
-jest.mock('../../../../app/processing/settlement/get-settlement')
+jest.mock('../../../../app/processing/statement/get-statement')
 const getStatement = require('../../../../app/processing/statement/get-statement')
 
 const processStatements = require('../../../../app/processing/process-statements')
@@ -11,15 +10,17 @@ const processStatements = require('../../../../app/processing/process-statements
 let statement
 
 describe('process statements', () => {
-  beforeAll(async () => {
-    await db.sequelize.truncate({
-      cascade: true,
-      restartIdentity: true
-    })
-  })
-
   beforeEach(async () => {
+    const { DATE } = require('../../../mock-components/mock-dates').UPDATED
+    jest.useFakeTimers().setSystemTime(DATE)
+
     statement = JSON.parse(JSON.stringify(require('../../../mock-objects/mock-statement')))
+
+    const SETTLEMENT = require('../../../mock-objects/mock-settlement')
+    const { STATEMENT } = require('../../../mock-objects/mock-schedule')
+
+    await db.settlement.create(SETTLEMENT)
+    await db.schedule.create(STATEMENT)
 
     getStatement.mockResolvedValue(statement)
   })
@@ -34,16 +35,13 @@ describe('process statements', () => {
   })
 
   describe('when schedulePendingSettlements returns record', () => {
-    beforeEach(async () => {
-    })
-
     describe('when valid statement', () => {
       test('should update started for schedule', async () => {
-        const scheduleBefore = await db.schedule.findAll({ raw: true })[0]
+        const scheduleBefore = await db.schedule.findOne({ where: { scheduleId: 1 }, raw: true })
 
         await processStatements()
 
-        const scheduleAfter = await db.schedule.findAll({ raw: true })[0]
+        const scheduleAfter = await db.schedule.findOne({ where: { scheduleId: 1 }, raw: true })
         expect(scheduleBefore.started).toBeNull()
         expect(scheduleAfter.started).toBeDefined()
       })
@@ -51,31 +49,31 @@ describe('process statements', () => {
       test('should update started to date now', async () => {
         await processStatements()
 
-        const schedule = await db.schedule.findAll({ raw: true })[0]
-        expect(schedule.started).toBe(new Date())
+        const schedule = await db.schedule.findOne({ where: { scheduleId: 1 }, raw: true })
+        expect(schedule.started).toStrictEqual(new Date())
       })
 
-      test('should call ffc-messaging sendMessage', async () => {
+      test('should call mockMessageSender().sendMessage', async () => {
         await processStatements()
-        expect(1).toHaveBeenCalled()
+        expect(mockMessageSender().sendMessage).toHaveBeenCalled()
       })
 
-      test('should call ffc-messaging sendMessage once', async () => {
+      test('should call mockMessageSender().sendMessage once', async () => {
         await processStatements()
-        expect(1).toHaveBeenCalledTimes(1)
+        expect(mockMessageSender().sendMessage).toHaveBeenCalledTimes(1)
       })
 
-      test('should call ffc-messaging sendMessage with message', async () => {
+      test('should call mockMessageSender().sendMessage with message', async () => {
         await processStatements()
-        expect(1).toHaveBeenCalledWith({ body: { a: 1 } })
+        expect(mockMessageSender().sendMessage).toHaveBeenCalledWith({ body: { a: 1 } })
       })
 
       test('should update schedule completed field', async () => {
-        const scheduleBefore = await db.schedule.findAll({ raw: true })[0]
+        const scheduleBefore = await db.schedule.findOne({ where: { scheduleId: 1 }, raw: true })
 
         await processStatements()
 
-        const scheduleAfter = await db.schedule.findAll({ raw: true })[0]
+        const scheduleAfter = await db.schedule.findOne({ where: { scheduleId: 1 }, raw: true })
         expect(scheduleBefore.completed).toBeNull()
         expect(scheduleAfter.completed).toBeDefined()
       })
@@ -83,24 +81,27 @@ describe('process statements', () => {
       test('should update schedule completed field to date now', async () => {
         await processStatements()
 
-        const schedule = await db.schedule.findAll({ raw: true })[0]
-        expect(schedule.completed).toBe(new Date())
+        const schedule = await db.schedule.findOne({ where: { scheduleId: 1 }, raw: true })
+        expect(schedule.completed).toStrictEqual(new Date())
       })
     })
 
     describe('when invalid statement', () => {
-      // invalidate
-      test('should not call ffc-messaging sendMessage', async () => {
+      beforeEach(async () => {
+        getStatement.mockResolvedValue({ ...statement, payments: [{ ...statement.payments[0], value: 0 }] })
+      })
+
+      test('should not call mockMessageSender().sendMessage', async () => {
         await processStatements()
-        expect(1).not.toHaveBeenCalled()
+        expect(mockMessageSender().sendMessage).not.toHaveBeenCalled()
       })
 
       test('should update schedule completed field', async () => {
-        const scheduleBefore = await db.schedule.findAll({ raw: true })[0]
+        const scheduleBefore = await db.schedule.findOne({ where: { scheduleId: 1 }, raw: true })
 
         await processStatements()
 
-        const scheduleAfter = await db.schedule.findAll({ raw: true })[0]
+        const scheduleAfter = await db.schedule.findOne({ where: { scheduleId: 1 }, raw: true })
         expect(scheduleBefore.completed).toBeNull()
         expect(scheduleAfter.completed).toBeDefined()
       })
@@ -108,26 +109,9 @@ describe('process statements', () => {
       test('should update schedule completed field to date now', async () => {
         await processStatements()
 
-        const schedule = await db.schedule.findAll({ raw: true })[0]
-        expect(schedule.completed).toBe(new Date())
+        const schedule = await db.schedule.findOne({ where: { scheduleId: 1 }, raw: true })
+        expect(schedule.completed).toStrictEqual(new Date())
       })
     })
   })
-})
-
-//   describe('for batch max size', () => {
-
-//   })
-
-//   describe('over settlementwaittime', () => {
-
-//   })
-
-//   describe('over scheduleProcessingMaxElapsedTime', () => {
-
-//   })
-// })
-
-test('a', () => {
-  expect(1).toBe(1)
 })
