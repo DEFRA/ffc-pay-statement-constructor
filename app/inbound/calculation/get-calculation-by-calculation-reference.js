17a381a33c93f44d9c760640ddfa9f41c8c61f96
Marc Templeton <56440327+marctemp@users.noreply.github.com>
2023-05-23 16:48:11 +0100

    Adding lock to Calculation table (#55)

--- app/inbound/calculation/get-calculation-by-calculation-reference.js
+++ app/inbound/calculation/get-calculation-by-calculation-reference.js
@@ -3,6 +3,7 @@ const db = require('../../data')
 const getCalculationByCalculationReference = async (calculationReference, transaction) => {
   return db.calculation.findOne({
     transaction,
+    lock: true,
     where: {
       calculationReference
     }
