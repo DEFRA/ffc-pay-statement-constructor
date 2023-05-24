bbd09d9df657aa3d08aae7e99121e11e5a6a3314
Marc Templeton <marcte@kainos.com>
2023-05-24 13:42:59 +0100

    removing funcs with notests, adding ticket to cover

--- app/processing/funding/get-funding-level.js
+++
@@ -1,5 +0,0 @@
-const getFundingLevel = (name) => {
-  return name.split(':')[1]?.trim() ?? ''
-}
-
-module.exports = getFundingLevel
--- app/processing/funding/get-funding-name.js
+++
@@ -1,5 +0,0 @@
-const getFundingName = (name) => {
-  return name.split(':')[0]?.trim() ?? ''
-}
-
-module.exports = getFundingName
