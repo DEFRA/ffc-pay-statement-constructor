a9fa98b0535fee6a6573bbb5ef636af3b153a6d7
Abidemi Adio <abidemi.adio@defra.gov.uk>
2023-10-31 12:17:01 +0000

    Changed claimId and AgreementNumber in claimAgreement table to String

--- app/data/models/claim-agreement.js
+++ app/data/models/claim-agreement.js
@@ -2,8 +2,8 @@ module.exports = (sequelize, DataTypes) => {
   const claimAgreement = sequelize.define('claimAgreement', {
     claimAgreementId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     frn: DataTypes.BIGINT,
-    claimId: DataTypes.INTEGER,
-    agreementNumber: DataTypes.INTEGER
+    claimId: DataTypes.STRING,
+    agreementNumber: DataTypes.STRING
   },
   {
     tableName: 'claimAgreements',
