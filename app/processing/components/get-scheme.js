d2930f34abd271a9648b2cd867108ce758d04735
Abidemi Adio <abidemi.adio@defra.gov.uk>
2023-11-01 00:01:05 +0000

    update IRO manual test feedback

--- app/processing/components/get-scheme.js
+++ app/processing/components/get-scheme.js
@@ -2,14 +2,27 @@ const { LONG_NAMES, SHORT_NAMES } = require('../../constants/scheme-names')
 const getAgreementNumberByClaimId = require('../agreementNumber/get-agreement-number-by-claim-id')
 
 const getScheme = async (year, frequency, agreementNumber, sourceSystem) => {
-  const docAgreementNumber = sourceSystem === SHORT_NAMES.SFIA ? await getAgreementNumberByClaimId(agreementNumber).agreementNumber : agreementNumber
+  if (sourceSystem === SHORT_NAMES.SFIA) {
+    const newAgreementNumber = await getAgreementNumberByClaimId(agreementNumber)
+    if (!newAgreementNumber) {
+      throw new Error(`SFIA claimId: ${agreementNumber} does not have corresponding agreementNumber`)
+    }
+
+    return {
+      name: LONG_NAMES[sourceSystem],
+      shortName: sourceSystem,
+      year: String(year),
+      frequency,
+      agreementNumber: newAgreementNumber.agreementNumber
+    }
+  }
 
   return {
     name: LONG_NAMES[sourceSystem],
     shortName: sourceSystem,
     year: String(year),
     frequency,
-    agreementNumber: docAgreementNumber
+    agreementNumber
   }
 }
 
